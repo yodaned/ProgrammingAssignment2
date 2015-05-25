24b5468a07fbcfe2b90f2039c24a6b29800e0f0e
nedk <yodatell13@gmail.com>
2015-05-24 19:17:15 -0500

    modified cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+library("corpcor")
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# This function creates a special "matrix" object that can cache its inverse.
+makeCacheMatrix <- function(X = matrix()) {
+        solinverse <- NULL
+        set <- function(Y){
+                X <<- Y
+                solinverse <<- NULL
+        }
+        get <- function() X
+        setsolinverse <- function(Inverse) solinverse <<- Inverse
+        getsolinverse <- function() solinverse
+        list(set=set,get=get,setsolinverse=setsolinverse,getsolinverse=getsolinverse)
 }
 
+# This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+# If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should 
+#retrieve the inverse from the cache
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(X, ...) {
+        solinverse <- X$getsolinverse()
+        if(!is.null(solinverse)){
+                message("MATRIX INVERSE LOADED FROM CACHE AS IT IS AVAILABLE")
+                return(solinverse)
+        }
+        message("MATRIX INVERSE COMPUTED AS IT IS NOT AVAILABLE IN CACHE")
+        data <- X$get()
+        solinverse <- pseudoinverse(data, ...)
+        X$setsolinverse(solinverse)
+        solinverse
 }
